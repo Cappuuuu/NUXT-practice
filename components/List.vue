0a0c11b663bfbd101ee8efc806b6f2312511eb61
Eric <ericz@cqcp.com.tw>
2021-01-05 14:34:30 +0800

    初始化 Nuxt

---
+++ components/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list__wrapper">
+    <ul>
+      <li class="list__item" v-for="(item,index) in detailData" :key="index"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+    }
+  },
+  props: ['detailData'],
+}
+</script>
+
+<style scoped>
+  .list__wrapper {
+    margin-top: 20px;
+    width: 500px;
+    height: 200px;
+    border: 5px solid #5081ad;
+    border-radius: 5px;
+  }
+
+  .list__item {
+    list-style: none;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <List :detailData="detailData"/>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ $axios }) {
+    const targetUrl = 'https://datacenter.taichung.gov.tw/swagger/OpenData/98f036d6-197f-4bcc-a0c9-e4363ac3cdd3'
+    return $axios.get(targetUrl)
+      .then((res) => {
+        return {
+          detailData: res.data,
+        }
+      }).catch((err) => {
+        console.log(err.message)
+      })
+  },
+  data () {
+    return {
+      detailData: [],
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 20px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
